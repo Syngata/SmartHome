13fe8eadf689a66fe3cd34fdb648eb5832614146
student <student@PC-10>
2018-12-03 19:43:21 +0100

    fghf

--- src/AppClient.java
+++ src/AppClient.java
@@ -3,29 +3,41 @@ public class AppClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//invoker
 		RemoteController rmt= new RemoteController();
+		
+		//recievers
+		
 		Light light= new Light();
 		Garage garage= new Garage();
 		StereoMethods strMet= new StereoMethods();
 		
-		LightOnCommand lightOnCom = new LightOnCommand(light);
-		LightOffCommanr lightOffCom= new LightOffCommanr(light);
-		GarageDoorCommand garDoorCom= new GarageDoorCommand(garage);
-		GarageDoorDown garDoorDown= new GarageDoorDown(garage);
+		//command objets on
+		LightOnCommand lightOnCom = new LightOnCommand(light);		
+		GarageDoorCommand garDoorCom= new GarageDoorCommand(garage);		
 		Stereo stOn = new Stereo(strMet);
-		StereoOff stOff= new StereoOff(strMet);
 		
+		//command objects off
+		StereoOff stOff= new StereoOff(strMet);
+		LightOffCommanr lightOffCom= new LightOffCommanr(light);
+		GarageDoorDown garDoorDown= new GarageDoorDown(garage);
+		
+		//programming buttons
 		rmt.setCommand(0, lightOnCom, lightOffCom);		
 		rmt.setCommand(1, garDoorCom, garDoorDown);		
 		rmt.setCommand(2, stOn, stOff);
 		
+		
 		System.out.println(rmt.toString());
 		
 		for (int i=0;i<rmt.intComOn.length;i++) {
-			System.out.println("**************SLOT "+ i +"*************");
+			System.out.println("**************SLOT "+ (i+1) +"*************");
 			rmt.buttonOnWasPressed(i);
 			rmt.buttonOffWasPressed(i);
 		}
+		
+		rmt.undoCom.undo();
 		
 		  		
 	}
--- src/RemoteController.java
+++ src/RemoteController.java
@@ -1,35 +1,42 @@
 
 public class RemoteController {
-	
+
 	IntCommand[] intComOn;
 	IntCommand[] intComOff;
 	IntCommand nc = new NoCommand();
+	IntCommand undoCom;
 	int slot;
-	
+
 	public RemoteController() {
 		// TODO Auto-generated constructor stub
-	intComOn= new IntCommand[7]; 
-	intComOff= new IntCommand[7];
-	
-	
-	for (int i =0; i <intComOn.length;i++) {
-		intComOn[i]= nc;
-		intComOff[i]=nc;
-		
+		intComOn = new IntCommand[7];
+		intComOff = new IntCommand[7];
+
+		for (int i = 0; i < intComOn.length; i++) {
+			intComOn[i] = nc;
+			intComOff[i] = nc;
+
+		}
+		undoCom = nc;
 	}
-	}
-	
+
 	public void setCommand(int slot, IntCommand comOn, IntCommand comOff) {
-		intComOn[slot]=comOn;
-		intComOff[slot]=comOff;
+		intComOn[slot] = comOn;
+		intComOff[slot] = comOff;
 	}
-	
+
 	public void buttonOnWasPressed(int slot) {
 		intComOn[slot].execute();
+		undoCom= intComOn[slot];
 	}
-	
+
 	public void buttonOffWasPressed(int slot) {
 		intComOff[slot].execute();
+		undoCom= intComOff[slot];
+	}
+	
+	public void undoBttn() {
+		undoCom.undo();
 	}
 
 	@Override
@@ -37,11 +44,11 @@ public class RemoteController {
 		// TODO Auto-generated method stub
 		StringBuffer sbuff = new StringBuffer();
 		sbuff.append("\n ********************** Remote Controller**********************\n ");
-		for(int i = 0; i<intComOn.length;i++) {
-			sbuff.append("[slot" + i + "]" + intComOn[i].getClass().getName() + "\t \t" + intComOff[i].getClass().getName()+"\n");
+		for (int i = 0; i < intComOn.length; i++) {
+			sbuff.append("[slot" + i + "]" + intComOn[i].getClass().getName() + "\t \t"
+					+ intComOff[i].getClass().getName() + "\n");
 		}
 		return sbuff.toString();
 	}
-	
-	
+
 }
